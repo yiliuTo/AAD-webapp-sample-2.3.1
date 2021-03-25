07813f025a52dd934e91e4661d46b514b432ecb5
Yi Liu <yiliu6@microsoft.com>
2021-03-25 14:50:51 +0800

    create example app

---
+++ src/main/java/com/example/demo/SampleAADApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@SpringBootApplication
+public class SampleAADApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAADApplication.class, args);
+	}
+
+}
